94639875a92d95bbb3b77f929488e2e337a229d3
Raneem <83226325+Raneem-pro@users.noreply.github.com>
2024-04-28 17:30:38 +0300

    the project start

---
+++ ToDo_Firebase_Array/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDo_Firebase_Array
+//
+//  Created by رنيم القرني on 19/10/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDo_Firebase_Array/ToDo_Firebase_ArrayApp.swift
@@ -0,0 +1,18 @@
+//
+//  ToDo_Firebase_ArrayApp.swift
+//  ToDo_Firebase_Array
+//
+//  Created by رنيم القرني on 19/10/1445 AH.
+//
+
+import SwiftUI
+
+
+@main
+struct ToDo_Firebase_ArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
