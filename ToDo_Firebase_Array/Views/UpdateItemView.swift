7444c59e1c7ed1be9fe1225934da6e45218e11f0
Raneem <83226325+Raneem-pro@users.noreply.github.com>
2024-04-28 18:20:41 +0300

    add new view for updating items

---
+++ ToDo_Firebase_Array/Views/UpdateItemView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateItemView.swift
+//  ToDo_Firebase_Array
+//
+//  Created by رنيم القرني on 19/10/1445 AH.
+//
+
+import SwiftUI
+
+struct UpdateItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateItemView()
+}
